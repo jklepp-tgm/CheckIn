9b4dd5214c312fcf9820bf02a7063e505ed835e4
Jakob Klepp <jklepp@student.tgm.ac.at>
2014-05-22 10:44:19 +0200

    first api method /FileList

--- CheckIn.py
+++ CheckIn.py
@@ -5,10 +5,16 @@ from flask import render_template
 
 app = Flask(__name__)
 
+apipath = u"/v0/"
+
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route(apipath + "/FileList")
+def filelist():
+    pass
 
 # start webservice
 if __name__ == "__main__":
-    app.run()
+    app.run('0.0.0.0')
